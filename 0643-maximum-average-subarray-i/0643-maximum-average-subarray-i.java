7e60a08c70b6c30941866077d1be7468084fd267
Ram Teja Kalluri <kalluriramteja@gmail.com>
2026-08-25 20:24:49 +0530

    Time: 5 ms (31.89%), Space: 69 MB (96.5%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) 
+    {
+        double max = 0,s=0;
+        for(int i=0;i<k;i++)
+        {
+            s=s+nums[i];
+        }
+        max=s/k;
+        for(int i=k;i<nums.length;i++)
+        {
+            s=s-nums[i-k]+nums[i];
+            max=Math.max(s/k,max);
+        }
+        return max;
+    }
+}
